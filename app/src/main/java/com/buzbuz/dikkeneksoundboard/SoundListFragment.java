3a706511fd67f8139ca049294e3c321b9266abb7
Nain57 <buzeau.kevin@gmail.com>
2015-07-26 17:38:43 +0200

    [DEV] Add play sound on item click

--- app/src/main/java/com/buzbuz/dikkeneksoundboard/SoundListFragment.java
+++ app/src/main/java/com/buzbuz/dikkeneksoundboard/SoundListFragment.java
@@ -4,14 +4,22 @@ import android.app.ListFragment;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.SearchView.OnQueryTextListener;
 import android.text.TextUtils;
+import android.util.Log;
 import android.support.v4.view.MenuItemCompat;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ListView;
+
+import java.io.IOException;
 
 /**
  * List fragment used to display the list of sounds
@@ -34,6 +42,8 @@ public class SoundListFragment extends ListFragment {
 
     private static final String SEARCH_SELECTION = SoundDatabaseHelper.SoundColumn.COLUMN_NAME_TITLE + " LIKE ?";
 
+    private Cursor mCurrentCursor;
+    private int mSoundPathColumnIndex = -1;
     /** Loader for the sound database cursor management */
     private final LoaderManager.LoaderCallbacks<Cursor> mSoundLoaderCallbacks = new LoaderManager.LoaderCallbacks<Cursor>() {
         @Override
@@ -47,11 +57,14 @@ public class SoundListFragment extends ListFragment {
 
         @Override
         public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-            ((SoundListCursorAdapter) getListAdapter()).swapCursor(data);
+            mCurrentCursor = data;
+            mSoundPathColumnIndex = mCurrentCursor.getColumnIndex(SoundDatabaseHelper.SoundColumn.COLUMN_NAME_PATH);
+            ((SoundListCursorAdapter) getListAdapter()).swapCursor(mCurrentCursor);
         }
 
         @Override
         public void onLoaderReset(Loader<Cursor> loader) {
+            mCurrentCursor = null;
             ((SoundListCursorAdapter) getListAdapter()).swapCursor(null);
         }
     };
@@ -77,6 +90,15 @@ public class SoundListFragment extends ListFragment {
         }
     };
 
+    private MediaPlayer mPlayer;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mPlayer = new MediaPlayer();
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    }
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -92,5 +114,42 @@ public class SoundListFragment extends ListFragment {
         inflater.inflate(R.menu.menu_main, menu);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.menu_search));
         searchView.setOnQueryTextListener(mQueryListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if (mPlayer.isPlaying()) mPlayer.stop();
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        if (mCurrentCursor != null) {
+            int oldPosition = mCurrentCursor.getPosition();
+            mCurrentCursor.moveToPosition(position);
+            playSelectedSound(mCurrentCursor.getString(mSoundPathColumnIndex));
+            mCurrentCursor.moveToPosition(oldPosition);
+        }
+    }
+
+    private void playSelectedSound(String filePath) {
+        if (mPlayer.isPlaying()) mPlayer.stop();
+
+        try {
+            AssetFileDescriptor soundFileDescriptor = getActivity().getAssets().openFd(filePath);
+            mPlayer.reset();
+            mPlayer.setDataSource(soundFileDescriptor.getFileDescriptor(),
+                                  soundFileDescriptor.getStartOffset(),
+                                  soundFileDescriptor.getLength());
+            mPlayer.prepare();
+            mPlayer.setVolume(1f, 1f);
+            mPlayer.start();
+            soundFileDescriptor.close();
+        } catch (IOException ex) {
+            Log.e(LOG_TAG, "Can't find sound file from asset " + filePath);
+        }
     }
 }
